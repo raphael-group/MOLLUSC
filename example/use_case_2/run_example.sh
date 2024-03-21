323ed9073370922b0acc4575d7f17eb2af5321d3
uym2 <ceciliamaitouyen@gmail.com>
2024-03-21 17:57:46 -0400

    remove redundant/obsolete files and reorganize example

---
+++ example/use_case_2/run_example.sh
@@ -0,0 +1,1 @@
+python ../../run_mollusc.py -c ../character_matrix.csv -t ../input_tree.nwk --delimiter comma -p ../mutation_priors.csv --nInitials 1 --randseeds 3103 -o mollusc_output.txt --timescale 215 -S ../input_locations.txt --divide --radius 5 -v
