00fd25220f9b0a870fbfdd8b95a4e1061d884076
uym2 <ceciliamaitouyen@gmail.com>
2024-03-22 13:47:35 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -28,16 +28,9 @@ param = {
         'include_package_data': True,
         'scripts': ['run_mollusc.py'],
         'zip_safe': True,
-        #'setup_requires': ['numpy'],
-        # SCS requires numpy < 1.2 to be installed: https://github.com/bodono/scs-python/issues/32#issuecomment-802977693
-        # cvxpy fails to install on python 3.12, requires SCS, ECOS and OSQP: https://github.com/cvxpy/cvxpy/issues/1367
-        # qdldl (dependency of osqp has no wheel for python 3.12): https://github.com/cvxpy/cvxpy/issues/2269
-        # osqp < 0.6.2 does not depend on qdldl to install cvxpy
-        # NOTE: if installing from python setup.py install, please `pip install cvxpy` first. 
-        'install_requires': ['cvxpy', 'treeswift>=1.1.39', 'Mosek', 'setuptools'], #, 'treeswift', 'Mosek', 'setuptools', 'scipy==1.11.4', 'scs==3.2.4.post1', 'clarabel==0.7.1'], #'ecos==2.0.13'], #'osqp==0.6.5'], # 'qdldl==0.1.7.post0'], #osqp>=0.6.1', 'ecos>=2', 'clarabel>=0.5.0', 'scs>=3.0', 'cvxpy'],
-        #'install_requires': ['numpy>=1.16', 'treeswift>=1.1.39', 'scipy>=1.3.1', 'cvxpy>=1.4', 'Mosek>=10.1.16', 'cmake>=3.18.0', 'setuptools', 'pybind11'], #'osqp==0.6.1'],
-        'keywords': '',
-        'long_description': """""",
+        'install_requires': ['scipy>=1.3.1','cvxpy>=1.4','treeswift>=1.1.39', 'Mosek>=10.1.16', 'setuptools'],        
+        'keywords': 'Spatial Lineage Tracing, Cell motility, Cell division, Maximum likelihood, Phylogeography',
+        'long_description': "Maximum Likelihood Estimation Of Lineage and Location Using Single Cell Spatial Lineage tracing Data",
         'classifiers': [
             "Environment :: Console",
             "Intended Audience :: Developers",
