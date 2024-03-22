1cf7dd5b4aa9e0a1af1962c99c549e09cbbf5049
uym2 <ceciliamaitouyen@gmail.com>
2024-03-22 15:40:55 -0400

    add example.zip

--- example/use_case_1/run_example.sh
+++ example/use_case_1/run_example.sh
@@ -1,1 +1,1 @@
-python ../../run_mollusc.py -c ../character_matrix.csv -t ../input_tree.nwk --delimiter comma -p ../mutation_priors.csv --nInitials 1 --randseeds 3103 -o mollusc_output.txt --timescale 215 -v
+python run_mollusc -c ../character_matrix.csv -t ../input_tree.nwk --delimiter comma -p ../mutation_priors.csv --nInitials 1 --randseeds 3103 -o mollusc_output.txt --timescale 215 -v
